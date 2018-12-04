744901db985d5a8dd025e77eb267c2b4759cf890
Wildhoney <adam.timberlake@gmail.com>
2018-12-04 09:45:25 +0000

    Only setting catch when error function passed

--- src/index.js
+++ src/index.js
@@ -15,22 +15,26 @@ const handleValue = (value, ok, error) => {
 };
 
 export const get = (value, ok, error) => {
-    if (isPromise(value)) return value.then(ok).catch(error);
+    if (isPromise(value)) return error ? value.then(ok).catch(error) : value.then(ok);
     return handleValue(value, ok, error);
 };
 
 export const all = (values, ok, error) => {
     if (hasPromise(values))
-        return Promise.all(values)
-            .then(ok)
-            .catch(error);
+        return error
+            ? Promise.all(values)
+                  .then(ok)
+                  .catch(error)
+            : Promise.all(values).then(ok);
     return handleValue(values, ok, error);
 };
 
 export const race = (values, ok, error) => {
     if (hasPromise(values))
-        return Promise.race(values)
-            .then(ok)
-            .catch(error);
+        return error
+            ? Promise.race(values)
+                  .then(ok)
+                  .catch(error)
+            : Promise.race(values).then(ok);
     return handleValue(values[0], ok, error);
 };
